de1e96d6dcae7d3f8788aa6eaec8736f70b5e7a3
Nilesh Deshmukh <dnilesh@intraedge.com>
2026-07-07 11:32:26 +0530

    feat: add CDK app scaffold and fix synth/deploy commands

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from stacks.app_stack import AppStack
+
+app = cdk.App()
+
+AppStack(app, "CircleciDemoDevStack", env=cdk.Environment(
+    account=None,  # Uses CDK_DEFAULT_ACCOUNT
+    region=None,   # Uses CDK_DEFAULT_REGION
+))
+
+app.synth()
